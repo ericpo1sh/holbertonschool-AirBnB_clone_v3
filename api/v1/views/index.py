021b8b6af01af167ddc1f99f401348a936a217c5
ericpo1sh <ericpo1sh@gmail.com>
2023-11-20 12:49:18 -0600

    Added API and set up flask to display status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-""" Docstring """
+""" index module """
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", strict_slashes=False)
+def return_status():
+    """ Return status  """
+    return jsonify({"status": "OK"})
