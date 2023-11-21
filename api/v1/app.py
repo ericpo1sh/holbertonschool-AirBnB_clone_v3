eea4548a047a04ff61851ebecb3655020cec6ed3
ericpo1sh <ericpo1sh@gmail.com>
2023-11-21 16:38:37 -0600

    testing app.py modules

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" app module """
+""" Flask App Module for Web """
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import storage
 from os import environ
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
