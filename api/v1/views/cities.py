131dc16b1236053d2055d7d797aa3aaace47889b
ericpo1sh <ericpo1sh@gmail.com>
2023-11-21 14:21:48 -0600

    Fixed __init__ files, sorted imports by alphabetical order

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Cities Module! """
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import abort, jsonify, make_response, request
 from models import storage
 from models.state import State
 from models.city import City
