3c08e9d1848faff241999c73285508ea2cdf93d4
ericpo1sh <ericpo1sh@gmail.com>
2023-11-21 14:11:14 -0600

    Modded files that werent modded

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Init module """
+""" Init modules """
 from flask import Blueprint
 
 
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Cities Module """
+""" Cities Module! """
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
