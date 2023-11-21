25a8edc7b3dd6269b8f5744034171a06855d009a
ericpo1sh <ericpo1sh@gmail.com>
2023-11-21 14:59:56 -0600

    reverted change back to original get function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,17 +72,10 @@ class FileStorage:
     def get(self, cls, id):
         """ Returns object based on the class and its ID, or None N/A """
         if cls in classes.values():
-            try:
-                requested_object = next(
-                    instance
-                    for instance in self.all(cls).values()
-                    if instance.id == id
-                )
-                return requested_object
-            except StopIteration:
-                return None
-        else:
-            return None
+            for obj in self.all(cls).values():
+                if obj.id == id:
+                    return obj
+        return None
 
     def count(self, cls=None):
         """ Returns count of number of instances of object in filestorage """
