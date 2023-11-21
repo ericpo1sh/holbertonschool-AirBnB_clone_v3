9713986d4f422f1767d388944f23ea205a553e6d
ericpo1sh <ericpo1sh@gmail.com>
2023-11-21 16:44:32 -0600

    added CORS module back into app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from flask import Flask, jsonify
 from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
